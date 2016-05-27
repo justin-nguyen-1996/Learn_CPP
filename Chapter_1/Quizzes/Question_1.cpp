8dc382d25dda9cbe89e27c7c7a01043cc3abf427
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-05-27 09:12:02 -0500

    Create Question_1.cpp
    Problem Definition

---
+++ Chapter_1/Quizzes/Question_1.cpp
@@ -0,0 +1,15 @@
+/*
+    Author:  Justin Nguyen
+    Created: 5/27/2016
+*/
+
+/*
+    Write a single-file program (named main.cpp) that reads two separate integers from the user, 
+    adds them together, and then outputs the answer. The program should use three functions:
+
+    1. A function named “readNumber” should be used to get (and return) a single integer from the user.
+    2. A function named “writeAnswer” should be used to output the answer. 
+       This function should take a single parameter and have no return value.
+    3. A main() function should be used to glue the above functions together. 
+*/
+
