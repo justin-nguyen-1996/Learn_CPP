df09f98bef5f35ea40fec2177b4d7bcb9d4224e3
Justin Nguyen <2014justinnguyen@gmail.com>
2016-05-29 00:49:30 -0500

    DOS vs UNIX line endings

--- Chapter_2/Lectures/Static_Casting/static.cpp
+++ Chapter_2/Lectures/Static_Casting/static.cpp
@@ -2,7 +2,7 @@
 
 int main() {
 	char letter {97};
-	std::cout << letter << std::endl;
+	std::cout << letter << "\n";
 	std::cout << static_cast<int> (letter) << std::endl;
 	std::cout << letter << std::endl;
 }
