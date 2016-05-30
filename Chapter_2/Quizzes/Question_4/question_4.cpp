2d7b81a1a3c1ab6f992c51a5e5c43b601761d9b0
Justin Nguyen <2014justinnguyen@gmail.com>
2016-05-30 00:56:30 -0500

    problem definition

---
+++ Chapter_2/Quizzes/Question_4/question_4.cpp
@@ -0,0 +1,17 @@
+/*
+    Author:  Justin Nguyen
+    Created: 5/30/2016
+
+    Write a program to simulate a ball being dropped from a tower
+
+    Sample output:
+    Enter the initial height of the tower in meters: 100
+    At 0 seconds, the ball is at height: 100 meters
+    At 1 seconds, the ball is at height: 95.1 meters
+    At x seconds, the ball is on the ground.
+*/
+
+int main() {
+	
+	return 0;
+}
