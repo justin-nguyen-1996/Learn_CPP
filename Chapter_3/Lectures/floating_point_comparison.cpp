b868de81da953ec22f06d358794cad2b2e59e9ea
Justin Nguyen <2014justinnguyen@gmail.com>
2016-05-31 00:08:07 -0500

    floating point comparison

---
+++ Chapter_3/Lectures/floating_point_comparison.cpp
@@ -0,0 +1,18 @@
+/*
+    Author:  Justin Nguyen
+    Created: 5/30/2016
+*/
+
+#include <cmath>
+
+int approxEqual(double a, double b, double absEpsilon, double relEpsilon) {
+	/* comparison toward absolute epsilon
+           used for comparing numbers near zero */
+	double diff = fabs(a - b);
+	if (diff <= absEpsilon) { return true; }
+
+	/* comparison toward relative epsilon
+           used as a backup comparison */
+	double greater = ((diff > 0) ? a : b);
+	return (diff <= greater * relEpsilon);
+}
