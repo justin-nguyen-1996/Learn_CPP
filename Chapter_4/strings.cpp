4219eebd0861f078ddec1e011e626e443c20db09
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-06-02 08:53:36 -0500

    strings.cpp - skeleton

---
+++ Chapter_4/strings.cpp
@@ -0,0 +1,22 @@
+/*
+    Author:  Justin Nguyen
+    Created: 6/2/2016
+*/
+
+/* 
+    Ask the user to enter their full name and their age. 
+    Output how many years they’ve lived for each letter in their name.
+    For simplicity, count spaces as a letter.
+
+    Sample output:
+    Enter your full name: John Doe
+    Enter your age: 46
+    You've lived 5.75 years for each letter in your name.
+*/
+
+#include <iostream>
+#include <string>
+
+int main() {
+    
+}
