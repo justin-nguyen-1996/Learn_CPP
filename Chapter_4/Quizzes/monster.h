7914394a726e53d509eb2b5ec117e8731b080b41
Justin Nguyen <2014justinnguyen@gmail.com>
2016-06-05 15:47:48 -0500

    moved struct after type

--- Chapter_4/Quizzes/monster.h
+++ Chapter_4/Quizzes/monster.h
@@ -6,18 +6,18 @@
 #ifndef MONSTER_H
 #define MONSTER_H
 
-struct Monster {
-	Monster_t type;
-	string name;
-	int health;
-};
-
 enum class Monster_t {
 	string "ogre",
 	string "dragon",
 	string "orc",
 	string "giant_spider",
 	string "slime"
+};
+
+struct Monster {
+	Monster_t type;
+	string name;
+	int health;
 };
 
 #endif 
