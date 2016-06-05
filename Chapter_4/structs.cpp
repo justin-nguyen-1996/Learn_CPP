5c2b728ba49a48926b52c53543b790bd684bd8c6
Justin Nguyen <2014justinnguyen@gmail.com>
2016-06-05 02:08:46 -0500

    passing args

--- Chapter_4/structs.cpp
+++ Chapter_4/structs.cpp
@@ -6,6 +6,16 @@
 #include <iostream>
 #include "struct_defs.h"
 
+Fraction pass_by_value(Fraction frac) {
+	frac.numerator = 42;
+	return frac;
+}
+
+Fraction pass_by_reference(Fraction* frac_ptr) {
+	frac_ptr -> numerator = 3.14;
+	return frac_ptr;
+}
+
 int main() {
 
 }
