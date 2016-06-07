197a21b24f375ba5c4d92c2bf782db8c20e8fc1d
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-06-07 11:28:35 -0500

    Create enums_switches.h

---
+++ Chapter_5/Control_Flow/enums_switches.h
@@ -0,0 +1,14 @@
+/*
+    Author:  Justin Nguyen
+    Created: 6/7/2016
+*/
+
+enum class Animal_t {
+        PIG,
+        CHICKEN,
+        GOAT,
+        CAT,
+        DOG,
+        OSTRICH
+};      
+
