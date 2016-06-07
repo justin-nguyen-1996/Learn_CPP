986c09b3346752dd140fc5c5454b8a8b835b347b
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-06-07 11:29:17 -0500

    getAnimalName

--- Chapter_5/Control_Flow/enums_switches.cpp
+++ Chapter_5/Control_Flow/enums_switches.cpp
@@ -5,6 +5,19 @@
 
 #include <iostream>
 #include <string>
+#include "enums_switches.h"
+
+std::string getAnimalName(Animal_t animal) {
+        switch(animal) {
+                case 0: return "pig"; break;
+                case 1: return "chicken"; break;
+                case 2: return "goat"; break;
+                case 3: return "cat"; break;
+                case 4: return "dog"; break;
+                case 5: return "ostrich"; break;
+                default: return "error: this animal doesn't exist"; break;
+        }
+}
 
 int main() {
         
