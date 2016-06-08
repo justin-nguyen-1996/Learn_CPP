1f30c8812a4bfcf04fc2b7c2fe7b46b9828ab8ce
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-06-08 09:47:00 -0500

    Problem Definition

---
+++ Chapter_5/Quizzes/hi_lo_game.cpp
@@ -0,0 +1,29 @@
+/*
+    Author:  Justin Nguyen
+    Created: 6/8/2016
+*/
+
+/*
+    Implement a game of hi-lo. First, pick a random integer between 1 and 100. 
+    The user is given 7 tries to guess the number (2 ^ 7 > 100). 
+    The program should tell them whether they guessed too high or too low. 
+    If the user guesses the right number, the program should tell them they won. 
+    If they run out of guesses, the program should tell them they lost, and what the correct number is. 
+    At the end of the game, the user should be asked if they want to play again. 
+    If the user doesn’t enter ‘y’ or ‘n’, ask them again.
+
+    Sample Output:
+    
+    Let's play a game.  I'm thinking of a number.  You have 7 tries to guess what it is.
+    Guess #1: 64
+    Your guess is too high.
+    Guess #2: 32
+    Your guess is too low.
+    Guess #3: 54
+    Your guess is too high.
+    Guess #4: 51
+    Correct! You win!
+    Would you like to play again (y/n)? y
+    .........
+    Sorry, you lose.  The correct number was 49.
+*/
--- Chapter_5/Quizzes/question_1.cpp
+++
@@ -1,5 +0,0 @@
-/*
-    Author:  Justin Nguyen
-    Created: 6/8/2016
-*/
-
