14867ed3f0ae9f5dda42c51dc85d3b8d65b9b0e5
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-06-08 14:00:04 -0500

    Create hi_lo_enums.h

---
+++ Chapter_5/Quizzes/hi_lo_enums.h
@@ -0,0 +1,13 @@
+#ifndef hi_lo_enums_h
+#define hi_lo_enums_h
+
+enum class Guess_Status {
+        LOW,
+        HIGH,
+        INCORRECT,
+        CORRECT,
+        VALID,
+        INVALID
+};
+
+#endif
