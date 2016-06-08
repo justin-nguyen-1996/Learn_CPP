2f995899d59e71b7815c391f0d8a119ecd5a7616
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-06-08 08:49:52 -0500

    Create meaningless_input.cpp

---
+++ Chapter_5/Lectures/Input_Validation/meaningless_input.cpp
@@ -0,0 +1,14 @@
+void printResult(double x, char op, double y)
+{
+    if (op == '+')
+        std::cout << x << " + " << y << " is " << x + y << '\n';
+    else if (op == '-')
+        std::cout << x << " - " << y << " is " << x - y << '\n';
+    else if (op == '*')
+        std::cout << x << " * " << y << " is " << x * y << '\n';
+    else if (op == '/')
+        std::cout << x << " / " << y << " is " << x / y << '\n';
+    else // otherwise tell the user what went wrong
+        std::cout << "Oops, that input is invalid.  Please try again.\n";
+    } // and try again
+}
