88103a2435ee58519deb76c2d0b11df0a975c28a
Justin Nguyen <2014justinnguyen@gmail.com>
2016-06-11 11:48:47 -0500

    swapping structure

--- Chapter_6/Lectures/Sorting/Bubble_Sort/bubble_sort.cpp
+++ Chapter_6/Lectures/Sorting/Bubble_Sort/bubble_sort.cpp
@@ -10,9 +10,14 @@ int main() {
 	int array {6, 3, 2, 9, 7, 1, 5, 4, 8};
 	int array_size = sizeof(array) / sizeof(array[0]);
 
-	for (start_i = 0; start_i < array_size - 1; start_i += 1) {
-		for (cur_i = start_i + 1; cur_i < array_size; cur_i += 1) {
-
+	for (int start_i = 0; start_i < array_size - 1; start_i += 1) {
+		for (int cur_i_low = start_i; 
+			cur_i_low < array_size - 1; cur_i_low += 1) {
+			
+			int cur_i_hi = cur_i_low + 1;
+			if (cur_i_low > cur_i_hi) {
+				std::swap(array[cur_i_low], array[cur_i_hi]);
+			}
 		}
 	}
 }
