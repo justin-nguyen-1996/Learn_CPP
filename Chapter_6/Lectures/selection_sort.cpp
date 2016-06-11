6748ba9f5081dbf42a923496f6c5af3cf2fcc5e5
Justin Nguyen <2014justinnguyen@gmail.com>
2016-06-11 11:27:28 -0500

    silly, forgot to swap values

--- Chapter_6/Lectures/selection_sort.cpp
+++ Chapter_6/Lectures/selection_sort.cpp
@@ -10,13 +10,14 @@ int main() {
 	int array[10] = {1, 5, 10, 24, -3, 16, 28};
 	int array_size = sizeof(array) / sizeof(array[0]);
 
-	for (int i = 0; i < array_size - 1; i += 1) {
+	for (int i = 0; i < array_size; i += 1) {
 		int smallest_index = i;
 		for (int j = i + 1; j < array_size; j += 1) {
 			if (array[j] < array[smallest_index]) {
 				smallest_index = j;
 			}
 		}
+		std::swap(array[i], array[smallest_index]);
 	}
 
 	std::string comma_space = "";
@@ -25,4 +26,5 @@ int main() {
 		std::cout << x;
 		comma_space = ", ";
 	}
+	std::cout << "\n";
 }
