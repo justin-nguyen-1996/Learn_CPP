ba1c290f28ff9b3cadd7e250aa495dc6452b8513
Justin Nguyen <2014justinnguyen@gmail.com>
2016-06-11 12:07:37 -0500

    don't move indices, start at zero each loop

--- Chapter_6/Lectures/Sorting/Bubble_Sort/bubble_sort.cpp
+++ Chapter_6/Lectures/Sorting/Bubble_Sort/bubble_sort.cpp
@@ -10,13 +10,13 @@ int main() {
 	int array[] = {6, 3, 2, 9, 7, 1, 5, 4, 8};
 	int array_size = sizeof(array) / sizeof(array[0]);
 
-	for (int start_i = 0; start_i < array_size - 1; start_i += 1) {
-		for (int cur_i_low = start_i; 
-			cur_i_low < array_size - 1; cur_i_low += 1) {
+	for (int count = 0; count < array_size - 1; count += 1) {
+		for (int lo_i = 0; 
+			lo_i < array_size - 1; lo_i += 1) {
 			
-			int cur_i_hi = cur_i_low + 1;
-			if (array[cur_i_low] > array[cur_i_hi]) {
-				std::swap(array[cur_i_low], array[cur_i_hi]);
+			int hi_i = lo_i + 1;
+			if (array[lo_i] > array[hi_i]) {
+				std::swap(array[lo_i], array[hi_i]);
 			}
 		}
 	}
