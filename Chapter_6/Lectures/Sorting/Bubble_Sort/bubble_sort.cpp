ada84b2e4ad61179b90d9445fe0d8e87704c9740
Justin Nguyen <2014justinnguyen@gmail.com>
2016-06-11 13:54:20 -0500

    modular

--- Chapter_6/Lectures/Sorting/Bubble_Sort/bubble_sort.cpp
+++ Chapter_6/Lectures/Sorting/Bubble_Sort/bubble_sort.cpp
@@ -6,26 +6,37 @@
 #include <iostream>
 #include <utility>
 
-int main() {
-	int array[] = {6, 3, 2, 9, 7, 1, 5, 4, 8};
-	int array_size = sizeof(array) / sizeof(array[0]);
+void bubbleSort(int array[], int array_size) {
 
 	for (int count = 0; count < array_size - 1; count += 1) {
-		for (int lo_i = 0; 
-			lo_i < array_size - 1; lo_i += 1) {
-			
+		for (int lo_i = 0; lo_i < array_size - 1; lo_i += 1) {
+
 			int hi_i = lo_i + 1;
 			if (array[lo_i] > array[hi_i]) {
 				std::swap(array[lo_i], array[hi_i]);
 			}
+
 		}
 	}
+}
 
+void printArray(int array[], int array_size) {
 	std::string comma_space;
-	for (el : array) {
+
+	for (int i = 0; i < array_size; i += 1) {
 		std::cout << comma_space;
-		std::cout << el;
+		std::cout << array[i];
 		comma_space = ", ";
 	}
+
 	std::cout << "\n";
+}
+
+int main() {
+	int array[] = {6, 3, 2, 9, 7, 1, 5, 4, 8};
+	int array_size = sizeof(array) / sizeof(array[0]);
+
+	bubbleSort(array, array_size);
+	printArray(array, array_size);
+
 }
