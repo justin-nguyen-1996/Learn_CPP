a3a87665805489c143e8df665d7350e14e771e02
Justin Nguyen <2014justinnguyen@gmail.com>
2016-06-10 23:07:51 -0500

    looping

--- Chapter_6/Lectures/selection_sort.cpp
+++ Chapter_6/Lectures/selection_sort.cpp
@@ -7,5 +7,14 @@
 #include <utility>
 
 int main() {
-	
+	int array[10] = {1, 5, 10, 24, -3, 16, 28};
+	int array_size = sizeof(array) / sizeof(array[0]);
+
+	for (int i = 0; i < array_size - 1; i += 1) {
+		for (int j = i + 1; j < array_size; j += 1) {
+			if (array[i] < array[smallest_index]) {
+
+			}
+		}
+	}
 }
