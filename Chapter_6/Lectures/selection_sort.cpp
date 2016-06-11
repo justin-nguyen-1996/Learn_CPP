039d5adae89c4cc0ba4bc28006b6f3fe50df497e
Justin Nguyen <2014justinnguyen@gmail.com>
2016-06-11 11:31:35 -0500

    no need for extra iteration

--- Chapter_6/Lectures/selection_sort.cpp
+++ Chapter_6/Lectures/selection_sort.cpp
@@ -10,7 +10,7 @@ int main() {
 	int array[10] = {1, 5, 10, 24, -3, 16, 28};
 	int array_size = sizeof(array) / sizeof(array[0]);
 
-	for (int i = 0; i < array_size; i += 1) {
+	for (int i = 0; i < array_size - 1; i += 1) {
 		int smallest_index = i;
 		for (int j = i + 1; j < array_size; j += 1) {
 			if (array[j] < array[smallest_index]) {
