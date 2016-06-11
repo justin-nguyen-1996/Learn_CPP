c750567ffcb7c87e2dece93a34ad5dd7da7c07ec
Justin Nguyen <2014justinnguyen@gmail.com>
2016-06-11 11:42:18 -0500

    looping structure

--- Chapter_6/Lectures/Sorting/Bubble_Sort/bubble_sort.cpp
+++ Chapter_6/Lectures/Sorting/Bubble_Sort/bubble_sort.cpp
@@ -7,5 +7,12 @@
 #include <utitlity>
 
 int main() {
-	
+	int array {6, 3, 2, 9, 7, 1, 5, 4, 8};
+	int array_size = sizeof(array) / sizeof(array[0]);
+
+	for (start_i = 0; start_i < array_size - 1; start_i += 1) {
+		for (cur_i = start_i + 1; cur_i < array_size; cur_i += 1) {
+
+		}
+	}
 }
