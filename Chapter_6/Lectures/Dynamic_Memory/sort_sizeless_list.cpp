b77a25ce8d02e11fe20b26217aaba55011ccd60f
Justin Nguyen <2014justinnguyen@gmail.com>
2016-06-19 16:16:52 -0500

    program definition

--- Chapter_6/Lectures/Dynamic_Memory/sort_sizeless_list.cpp
+++ Chapter_6/Lectures/Dynamic_Memory/sort_sizeless_list.cpp
@@ -3,6 +3,24 @@
     Created: 6/19/2016
 */
 
+/*
+	Sample Output:
+
+	How many names would you like to enter? 5
+	Enter name #1: Jason
+	Enter name #2: Mark
+	Enter name #3: Alex
+	Enter name #4: Chris
+	Enter name #5: John
+
+	Here is your sorted list:
+	Name #1: Alex
+	Name #2: Chris
+	Name #3: Jason
+	Name #4: John
+	Name #5: Mark
+*/
+
 #include <iostream>
 #include <string>
 
