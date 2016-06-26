07fdfb8df8208b4ac6f06fcf6aaa7157285ef7fa
Justin Nguyen <2014justinnguyen@gmail.com>
2016-06-26 00:27:54 -0500

    get input

--- Chapter_6/Lectures/For_Each_Loops/for_each_example.cpp
+++ Chapter_6/Lectures/For_Each_Loops/for_each_example.cpp
@@ -2,10 +2,17 @@
 #include <string>
 
 int main() {
-	const std::string names_list[] = { 
+	using namespace std;
+	const string names_list[] = { 
 		"Alex", "Betty", "Caroline", 
 		"Dave", "Emily", "Fred",
 		"Greg", "Holly" 
 	};
+
+	cout << "Enter a name: ";
+	string name;
+	getline(cin, name);
+	cin.ignore(32767, '\n');
+
 
 }
