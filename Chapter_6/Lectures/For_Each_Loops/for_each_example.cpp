46fdc6baaa0549fe88e5dcc29a9d82e3edf86383
Justin Nguyen <2014justinnguyen@gmail.com>
2016-06-26 00:17:24 -0500

    program structure

---
+++ Chapter_6/Lectures/For_Each_Loops/for_each_example.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	const std::string names_list[] = { 
+		"Alex", "Betty", "Caroline", 
+		"Dave", "Emily", "Fred",
+		"Greg", "Holly" 
+	};
+
+}
