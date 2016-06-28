ef147c2005f4686ee334c9ff162d7cf98a9ca06b
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-06-28 10:39:20 -0500

    Problem Definition

--- Chapter_6/Quizzes/quiz_1.cpp
+++ Chapter_6/Quizzes/quiz_1.cpp
@@ -3,6 +3,13 @@
     Created: 6/28/16
 */
 
+/* Player can hold 3 types of items: health potions, torches, and arrows. 
+   Create an enum to identify the different types of items. 
+   Create a fixed array to store the number of each item the player is carrying 
+   The player should start with 2 health potions, 5 torches, and 10 arrows. 
+   Write a function called countTotalItems() that returns how many items the player has in total. 
+   Have your main() function print the output of countTotalItems(). */
+
 #include <iostream>
 
 int main() {
