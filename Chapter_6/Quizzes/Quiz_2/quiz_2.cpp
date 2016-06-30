7ede46d74869cca089b6ac09544d6bf32626d524
Justin Nguyen <2014justinnguyen@gmail.com>
2016-06-29 21:47:28 -0500

    setting up input structure

--- Chapter_6/Quizzes/Quiz_2/quiz_2.cpp
+++ Chapter_6/Quizzes/Quiz_2/quiz_2.cpp
@@ -3,8 +3,48 @@
 	Created: 6/29/2016
 */
 
+/* Create a struct that holds a student’s name and grade 
+	   (on a scale of 0-100). 
+   Ask the user how many students they want to enter. 
+   Dynamically allocate an array to hold all of the students. 
+   Then prompt the user for each name and grade. 
+   Once the user has entered all the names and grades, 
+       sort the list by grade (highest first). 
+   Then print all the names and grades in sorted order.
+
+For the following input:
+
+Joe
+82
+Terry
+73
+Ralph
+4
+Alex
+94
+Mark
+88
+
+The output should look like this:
+
+Alex got a grade of 94
+Mark got a grade of 88
+Joe got a grade of 82
+Terry got a grade of 73
+Ralph got a grade of 4
+*/
+
+
 #include <iostream>
+#include "error_type.h"
+#include "quiz_2.h"
 
 int main() {
-	
+	std::cout << "How many people do you want to enter?";
+	int num_people;
+	do {
+		std::cin >> num_people;
+	} (while testBadInput() == ErrorType::FAILURE);
+
+	grade_t person 
 }
--- Chapter_6/Quizzes/Quiz_2/quiz_2.h
+++ Chapter_6/Quizzes/Quiz_2/quiz_2.h
@@ -1,9 +1,11 @@
 #ifndef QUIZ_2
 #define QUIZ_2
 
+//#include <string>
+
 struct grade_t {
-	std::string name,
-	int grade
+	std::string name;
+	int grade;
 };
 
 #endif
