b0bdc5ca50de8b5003d532ce11faa0cbfae15f8f
Justin Nguyen <2014justinnguyen@gmail.com>
2016-06-30 00:10:20 -0500

    program works

--- Chapter_6/Quizzes/Quiz_2/quiz_2.cpp
+++ Chapter_6/Quizzes/Quiz_2/quiz_2.cpp
@@ -39,10 +39,10 @@ Ralph got a grade of 4
 #include "error_type.h"
 #include "quiz_2.h"
 
-ErrorType testBadInput() {
+ErrorType testBadInput(int input) {
 	using namespace std;
 
-	if (cin.fail()) {
+	if (cin.fail()   ||   input < 0   ||   input > 100) {
 		cin.clear();
 		cin.ignore(32767, '\n');
 		cout << "Error. Enter a valid integer (0-100): ";
@@ -61,7 +61,7 @@ int main() {
 	int num_students;
 	do {
 		cin >> num_students;
-	} while (testBadInput() == ErrorType::FAILURE);
+	} while (testBadInput(num_students) == ErrorType::FAILURE);
 
 	/* dynamic array holding all students */
 	student* student_list = new student[num_students];
@@ -77,7 +77,7 @@ int main() {
 		int grade;
 		do {
 			cin >> grade;
-		} while (testBadInput() == ErrorType::FAILURE);
+		} while (testBadInput(grade) == ErrorType::FAILURE);
 
 		student_list[student_i] = {name, grade};
 		//student_list[student_i].name = name;
