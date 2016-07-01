d387cf54d91aa9d35745ee03dac5c72fa69a9dbe
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-07-01 16:35:02 -0500

    Create card_ranks.h

---
+++ Chapter_6/Quizzes/Quiz_5/card_ranks.h
@@ -0,0 +1,22 @@
+#ifndef card_ranks
+#define card_ranks
+
+namespace CardRank_t {
+    enum CardRank_t {
+        TWO = 2,
+        THREE,
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        JACK,
+        QUEEN,
+        KING,
+        ACE
+    };
+}
+
+#ifndef
