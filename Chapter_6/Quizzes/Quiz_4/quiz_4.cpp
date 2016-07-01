0ec98a2f26d7e73c75be1310efbdafcaec6a0a6e
Justin Nguyen <2014justinnguyen@gmail.com>
2016-06-30 21:46:05 -0500

    problem definition

--- Chapter_6/Quizzes/Quiz_4/quiz_4.cpp
+++ Chapter_6/Quizzes/Quiz_4/quiz_4.cpp
@@ -3,6 +3,11 @@
 	Created: 6/30/2016
 */
 
+/* Write a function to print a C-style string (character by character). 
+   Use a pointer to step through and print that character. 
+   Stop when you hit a null terminator. 
+   Write a main function that tests the function. */
+
 int main() {
 	
 }
