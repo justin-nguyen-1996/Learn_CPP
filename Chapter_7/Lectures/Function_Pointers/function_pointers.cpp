b7ccff49dd73074e7dadd96611fe857bf1522836
Justin Nguyen <2014justinnguyen@gmail.com>
2016-07-04 01:07:35 -0500

    asking for input

--- Chapter_7/Lectures/Function_Pointers/function_pointers.cpp
+++ Chapter_7/Lectures/Function_Pointers/function_pointers.cpp
@@ -6,5 +6,16 @@
 #include <iostream>
 
 int main() {
-	
+	using namespace std;
+
+	int int_a; int int_b;
+	char op;
+	do {
+		cout << "Enter an integer: ";
+		cin >> a;
+		cout << "Enter an integer: ";
+		cin >> b;
+		cout << "Enter a mathematical operation (+,-,*,/): ";
+		cin >> op;
+	} while (testBadInput == ERROR);
 }
