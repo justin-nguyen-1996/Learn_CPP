298c41fe7cca318af59bf64b4133b09dcbf13f58
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-07-05 09:05:16 -0500

    filled out, pending tests

--- Chapter_7/Lectures/Recursion/factorial_recursive.cpp
+++ Chapter_7/Lectures/Recursion/factorial_recursive.cpp
@@ -5,8 +5,17 @@
 
 #include <iostream>
 
-
+int factorial(int x) {
+    if (x <= 1) { return 1; }
+    
+    return x * factorial(x-1);
+}
 
 int main() {
-    
+    std::cout << "Factorial(0): " << factorial(0) << "\n";
+    std::cout << "Factorial(1): " << factorial(0) << "\n";
+    std::cout << "Factorial(2): " << factorial(0) << "\n";
+    std::cout << "Factorial(3): " << factorial(0) << "\n";
+    std::cout << "Factorial(7): " << factorial(0) << "\n";
 }
+
