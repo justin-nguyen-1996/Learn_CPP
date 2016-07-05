fffa0ab66f6af88987ad828374ac2aacb1df704a
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-07-05 09:24:21 -0500

    wrote fibonacci iterative

--- Chapter_7/Lectures/Recursion/Fibonacci/fibonacci_iterative.cpp
+++ Chapter_7/Lectures/Recursion/Fibonacci/fibonacci_iterative.cpp
@@ -5,6 +5,21 @@
 
 #include <iostream>
 
+int fibonacci(int x) {
+    if (x <= 2) { return 2; }
+    
+    int two_ago = 1;
+    int one_ago = 1;
+    int cur;
+    for (int counter = 2; counter < x; counter += 1) {
+        cur = one_ago + two ago;
+        two_ago = one_ago;
+        one_ago = cur;
+    }
+    
+    return cur;
+}
+
 int main() {
 
 }
