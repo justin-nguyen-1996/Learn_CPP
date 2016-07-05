a1aa016f71be8a7e34658e0eca252a9777f0e22d
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-07-05 09:13:50 -0500

    filled out, pending tests

--- Chapter_7/Lectures/Recursion/fibonacci_recursive.cpp
+++ Chapter_7/Lectures/Recursion/fibonacci_recursive.cpp
@@ -5,7 +5,18 @@
 
 #include <iostream>
 
-int main() {
+int fibonacci(int x) {
+    if (x == 0) { return 0; }
+    else if (x == 1) { return 1; }
+    
+    return fibonacci(x-1) + fibonacci(x-2);
+}
 
+int main() {
+    std::cout << "Fibonacci(0): " << fibonacci(0) << "\n";
+    std::cout << "Fibonacci(1): " << fibonacci(1) << "\n";
+    std::cout << "Fibonacci(2): " << fibonacci(2) << "\n";
+    std::cout << "Fibonacci(3): " << fibonacci(3) << "\n";
+    std::cout << "Fibonacci(7): " << fibonacci(7) << "\n";
 }
 
