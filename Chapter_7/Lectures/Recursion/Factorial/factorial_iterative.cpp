3ce35dd4cbb7aa645947badc1b8a97035e16a59a
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-07-05 09:09:19 -0500

    filled out, pending tests

--- Chapter_7/Lectures/Recursion/Factorial/factorial_iterative.cpp
+++ Chapter_7/Lectures/Recursion/Factorial/factorial_iterative.cpp
@@ -5,6 +5,21 @@
 
 #include <iostream>
 
-int main() {
-
+int factorial(int x) {
+    int res = 1;
+    while (x > 1) {
+        res *= x;
+        x -= 1;
+    }
+    
+    return res;
 }
+
+int main() {
+    std::cout << "Factorial(0): " << factorial(0) << "\n";
+    std::cout << "Factorial(1): " << factorial(1) << "\n";
+    std::cout << "Factorial(2): " << factorial(2) << "\n";
+    std::cout << "Factorial(3): " << factorial(3) << "\n";
+    std::cout << "Factorial(7): " << factorial(7) << "\n";
+}
+
