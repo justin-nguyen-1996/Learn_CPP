f1210a8b17bb6ab8fbe2705c1e9e069c23419b93
Justin Nguyen <2014justinnguyen@gmail.com>
2016-07-07 01:59:07 -0500

    works, finished

--- Chapter_7/Lectures/Recursion/Fibonacci/Recursive_Version/fibonacci_recursive.cpp
+++ Chapter_7/Lectures/Recursion/Fibonacci/Recursive_Version/fibonacci_recursive.cpp
@@ -17,6 +17,9 @@ int main() {
     std::cout << "Fibonacci(1): " << fibonacci(1) << "\n";
     std::cout << "Fibonacci(2): " << fibonacci(2) << "\n";
     std::cout << "Fibonacci(3): " << fibonacci(3) << "\n";
+    std::cout << "Fibonacci(4): " << fibonacci(4) << "\n";
+    std::cout << "Fibonacci(5): " << fibonacci(5) << "\n";
+    std::cout << "Fibonacci(6): " << fibonacci(6) << "\n";
     std::cout << "Fibonacci(7): " << fibonacci(7) << "\n";
 }
 
