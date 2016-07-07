847255564abbe9cf9beb9d40e539947f5a074fc7
Justin Nguyen <2014justinnguyen@gmail.com>
2016-07-07 01:55:54 -0500

    works, finished

--- Chapter_7/Lectures/Recursion/Fibonacci/Iterative_Version/fibonacci_iterative.cpp
+++ Chapter_7/Lectures/Recursion/Fibonacci/Iterative_Version/fibonacci_iterative.cpp
@@ -6,12 +6,13 @@
 #include <iostream>
 
 int fibonacci(int x) {
-    if (x <= 2) { return 2; }
+	if      (x == 0) { return 0; }
+	else if (x == 1) { return 1; }
     
-    int two_ago = 1;
+    int two_ago = 0;
     int one_ago = 1;
     int cur;
-    for (int counter = 2; counter < x; counter += 1) {
+    for (int counter = 1; counter < x; counter += 1) {
         cur = one_ago + two_ago;
         two_ago = one_ago;
         one_ago = cur;
@@ -25,6 +26,9 @@ int main() {
     std::cout << "Fibonacci(1): " << fibonacci(1) << "\n";
     std::cout << "Fibonacci(2): " << fibonacci(2) << "\n";
     std::cout << "Fibonacci(3): " << fibonacci(3) << "\n";
+    std::cout << "Fibonacci(4): " << fibonacci(4) << "\n";
+    std::cout << "Fibonacci(5): " << fibonacci(5) << "\n";
+    std::cout << "Fibonacci(6): " << fibonacci(6) << "\n";
     std::cout << "Fibonacci(7): " << fibonacci(7) << "\n";
 }
 
