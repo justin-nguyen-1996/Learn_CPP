bbb73e71cfaadd9c5e3f20e88a740509bfbdb001
Justin Nguyen <2014justinnguyen@gmail.com>
2016-07-07 02:08:27 -0500

    works, finished

--- Chapter_7/Lectures/Recursion/Sum_Of_Digits/Recursive_Version/sum_of_digits_recursive.cpp
+++ Chapter_7/Lectures/Recursion/Sum_Of_Digits/Recursive_Version/sum_of_digits_recursive.cpp
@@ -7,17 +7,17 @@
 
 int sumOfDigits(int x) {
     if (x < 10) { return x % 10; }
-    return (x % 10) + (x / 10);
+    return (x % 10) + sumOfDigits(x / 10);
 }
 
 int main() {
-    std::cout << "sumOfDigits(0110)" << sumOfDigits(0110) << "\n";
-    std::cout << "sumOfDigits(2110)" << sumOfDigits(2110) << "\n";
-    std::cout << "sumOfDigits(0113)" << sumOfDigits(0113) << "\n";
-    std::cout << "sumOfDigits(1423)" << sumOfDigits(1423) << "\n";
-    std::cout << "sumOfDigits(12)" << sumOfDigits(12) << "\n";
-    std::cout << "sumOfDigits(12349746294)" << sumOfDigits(12349746294) << "\n";
-    std::cout << "sumOfDigits(93427)" << sumOfDigits(93427) << "\n";
-    std::cout << "sumOfDigits(357)" << sumOfDigits(357) << "\n";
+    std::cout << "sumOfDigits(0110): " << sumOfDigits(110) << "\n";
+    std::cout << "sumOfDigits(2110): " << sumOfDigits(2110) << "\n";
+    std::cout << "sumOfDigits(0113): " << sumOfDigits(113) << "\n";
+    std::cout << "sumOfDigits(1423): " << sumOfDigits(1423) << "\n";
+    std::cout << "sumOfDigits(12): " << sumOfDigits(12) << "\n";
+    std::cout << "sumOfDigits(123497494): " << sumOfDigits(123497494) << "\n";
+    std::cout << "sumOfDigits(93427): " << sumOfDigits(93427) << "\n";
+    std::cout << "sumOfDigits(357): " << sumOfDigits(357) << "\n";
 }
 
