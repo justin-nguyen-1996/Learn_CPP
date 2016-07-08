60adef77fee98f73c4e56b552085bc462130c43d
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-07-08 12:23:02 -0500

    if structure of binary search

--- Chapter_7/Quizzes/Binary_Search_Iterative/binary_search.cpp
+++ Chapter_7/Quizzes/Binary_Search_Iterative/binary_search.cpp
@@ -4,6 +4,19 @@
 */
 
 #include <iostream>
+#include <array>
+
+int binarySearch(std::array<int> num_list, int target) {
+    while(1) {
+        if (num_list[index] > target) {
+            
+        } else if (num_list[index] < target) {
+            
+        } else { // num_list == target
+            
+        }
+    }
+}
 
 int main() {
 
