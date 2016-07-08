15eaeae04e0fcac80eafc9608d37a666900b54a9
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-07-08 14:00:14 -0500

    Create intro_to_OOP.cpp

---
+++ Chapter_8/Lectures/intro_to_OOP.cpp
@@ -0,0 +1,17 @@
+/*
+    Author:  Justin Nguyen
+    Created: 7/8/2016
+*/
+
+1. Definition of Object Oriented Programming
+    --> Objects have two major components
+        --> properties
+        --> behavior
+    --> (OOP) gives us the ability to design an “object”, which ties together 
+        both properties and behaviors into a self-contained, reusable package
+    --> this allows programs to be written in a more modular fashion, 
+        which makes them easier to understand and reuse
+    --> objects provide a more intuitive way to work with data by allowing us to 
+        define how we interact with the objects, and how they interact with other objects
+    
+    
