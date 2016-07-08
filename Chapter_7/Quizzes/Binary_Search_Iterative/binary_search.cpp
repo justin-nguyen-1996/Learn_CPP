f42200e715bfda540fcfb73c873d18dca38a3fa0
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-07-08 12:48:19 -0500

    filled out main, pending tests

--- Chapter_7/Quizzes/Binary_Search_Iterative/binary_search.cpp
+++ Chapter_7/Quizzes/Binary_Search_Iterative/binary_search.cpp
@@ -10,7 +10,7 @@ int binarySearch(std::array<int> &num_list, int target) {
     int hi = num_list.size() - 1;
     int lo = 0;
     int midpt = (hi - lo) / 2;
-    while(1) {
+    while (lo <= hi) {
         if (num_list[midpt] > target) {
             hi = midpt - 1;
         } else if (num_list[midpt] < target) {
@@ -21,8 +21,25 @@ int binarySearch(std::array<int> &num_list, int target) {
         
         midpt = (hi - lo) / 2;
     }
+    
+    return -1;
 }
 
 int main() {
-
+    std::array<int,10> num_list
+        { 1, 5, 19, 34, 52, 100, 234, 365, 366, 367 };
+    std::array<int,10> target_list
+        { -1, 0, 1, 34, 35, 235, 234, 234, 267, 5 };
+    
+    for (const auto &target : target_list) {
+        int index = binarySearch(num_list, target);
+        std::cout << "List of nums: " << num_list << "\n";
+        if (index != -1) {
+            std::cout << "Binary Search found " << target 
+                      << " at index " << index << "\n";
+        } else {
+            std::cout << "Element " << target
+                      << " is not in the list\n";
+        }
+    }
 }
