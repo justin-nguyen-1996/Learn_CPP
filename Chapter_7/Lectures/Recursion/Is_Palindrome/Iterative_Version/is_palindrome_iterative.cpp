7ee6ee5fe87a83d14f9aa50fc221925d50f5744b
Justin Nguyen <2014justinnguyen@gmail.com>
2016-07-07 23:07:11 -0500

    done

---
+++ Chapter_7/Lectures/Recursion/Is_Palindrome/Iterative_Version/is_palindrome_iterative.cpp
@@ -0,0 +1,15 @@
+/*
+	Author:  Justin Nguyen
+	Created: 7/7/2016
+*/
+
+#include <iostream>
+
+int main() {
+	/*
+		Algorithm is simple (same as reverse string)
+		Loop through until halfway
+		Check if end and beginning chars are the same
+	*/
+}
+
