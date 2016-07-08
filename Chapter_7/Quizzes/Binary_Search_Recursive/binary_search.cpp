edd6ddd874518243b267964b367f9578b0aa098a
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-07-08 13:29:23 -0500

    filled out binarySearch

--- Chapter_7/Quizzes/Binary_Search_Recursive/binary_search.cpp
+++ Chapter_7/Quizzes/Binary_Search_Recursive/binary_search.cpp
@@ -7,12 +7,14 @@
 #include <vector>
 
 int binarySearch(std::vector<int> num_list, int target, int max, int min) {
+    if (min > max) { return -1; }
+    
     int mid = min + (max - min) / 2;
     
     if (num_list[mid] > target) {
-        return binarySearch()
+        return binarySearch(num_list, target, mid - 1, min);
     } else if (num_list[mid] < target) {
-        
+        return binarySearch(num_list, target, max, mid + 1);
     } else { // num_list[mid] == target
         return mid;
     }
