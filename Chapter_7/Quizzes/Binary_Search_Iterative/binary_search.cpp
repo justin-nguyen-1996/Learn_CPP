2cead1ea98315ed225d7930f85bdc834c2ceab74
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-07-08 12:28:43 -0500

    filled out binarySearch

--- Chapter_7/Quizzes/Binary_Search_Iterative/binary_search.cpp
+++ Chapter_7/Quizzes/Binary_Search_Iterative/binary_search.cpp
@@ -6,15 +6,20 @@
 #include <iostream>
 #include <array>
 
-int binarySearch(std::array<int> num_list, int target) {
+int binarySearch(std::array<int> &num_list, int target) {
+    int hi = num_list.size() - 1;
+    int lo = 0;
+    int midpt = (hi - lo) / 2;
     while(1) {
-        if (num_list[index] > target) {
-            
-        } else if (num_list[index] < target) {
-            
+        if (num_list[midpt] > target) {
+            hi = midpt - 1;
+        } else if (num_list[midpt] < target) {
+            lo = midpt + 1;
         } else { // num_list == target
-            
+            return midpt;
         }
+        
+        midpt = (hi - lo) / 2;
     }
 }
 
