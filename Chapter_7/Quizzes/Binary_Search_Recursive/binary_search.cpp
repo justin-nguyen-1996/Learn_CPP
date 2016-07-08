45ee4c2d939832a8cef407b7d099d3d1a1bc5391
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-07-08 13:12:02 -0500

    binary search structure

--- Chapter_7/Quizzes/Binary_Search_Recursive/binary_search.cpp
+++ Chapter_7/Quizzes/Binary_Search_Recursive/binary_search.cpp
@@ -6,7 +6,17 @@
 #include <iostream>
 #include <vector>
 
-int binarySearch(
+int binarySearch(std::vector<int> num_list, int target, int max, int min) {
+    int mid = min + (max - min) / 2;
+    
+    if (num_list[mid] > target) {
+        return binarySearch()
+    } else if (num_list[mid] < target) {
+        
+    } else { // num_list[mid] == target
+        return mid;
+    }
+}
 
 int main() {
 
