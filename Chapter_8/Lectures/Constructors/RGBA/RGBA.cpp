82dcf5a3338633276aabf30c3840791127da7f3b
Justin Nguyen <2014justinnguyen@gmail.com>
2016-07-10 13:20:52 -0500

    filled out constructor

---
+++ Chapter_8/Lectures/Constructors/RGBA/RGBA.cpp
@@ -0,0 +1,23 @@
+/*
+	Author:  Justin Nguyen
+	Created: 7/10/2016
+*/
+
+#include <iostream>
+#include <cstding>
+
+class RGBA {
+	private:
+		uint8_t red, green, blue, alpha;
+	public:
+		RGBA(red = 0, green = 0, blue = 0, alpha = 255)
+		 : this->red { red }, this->green { green }
+		   this->blue { blue }, this -> alpha { alpha }
+		{
+			
+		}
+};
+
+int main() {
+	
+}
