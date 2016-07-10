85b645e6225a5a67c8ac1bb17d6d075a0dee7c79
Justin Nguyen <2014justinnguyen@gmail.com>
2016-07-09 20:07:05 -0500

    stack class

---
+++ Chapter_8/Lectures/Access_Specifiers/Stack/stack.cpp
@@ -0,0 +1,32 @@
+/*
+	Author:  Justin Nguyen
+	Created: 7/9/2016
+*/
+
+#include <iostream>
+#include <array>
+
+class Stack {
+	private:
+		std::array<int, 10> num_list {};
+		int stack_length = 0;
+	public:
+		void reset() {
+			stack_length = 0;
+			num_list = {};
+		}
+
+		bool push(int num) {
+			if (stack_length == 10) { return false; }
+			num_list[stack_length + 1] = num;
+		}
+
+		int pop(int num) {
+			if (stack_length == 0) { return -1; }
+			return num_list[stack_length];
+		}
+};
+
+int main() {
+	
+}
