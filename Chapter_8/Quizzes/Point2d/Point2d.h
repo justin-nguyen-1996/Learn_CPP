5612d8af142234c0a95fdffd29a1ee1660d9f38d
Justin Nguyen <2014justinnguyen@gmail.com>
2016-07-24 22:14:12 -0500

    moving definitions to .cpp file

--- Chapter_8/Quizzes/Point2d/Point2d.h
+++ Chapter_8/Quizzes/Point2d/Point2d.h
@@ -6,17 +6,17 @@
 #ifndef POINT2D_H
 #define POINT2D_H
 
+#include <iostream>
+
 class Point2d {
 	private:
 		double m_x = 0.0;
 		double m_y = 0.0;
 	public:
-		Point2d() {}
+		Point2d();
 		Point2d(double x, double y) : m_x(x), m_y(y) {}
 		
-		void print() {
-			std::cout << "(" << m_x << ", " << m_y << ")" << "\n";
-		}
+		void print();
 		
 		double getX() { return m_x; }
 		double getY() { return m_y; }
