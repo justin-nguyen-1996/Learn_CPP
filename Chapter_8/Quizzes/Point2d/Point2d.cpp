cf66ce7a0104139ae65890936aabc450cf15b0f7
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-07-25 17:15:48 -0500

    updated main()

--- Chapter_8/Quizzes/Point2d/Point2d.cpp
+++ Chapter_8/Quizzes/Point2d/Point2d.cpp
@@ -11,7 +11,7 @@ void Point2d::print() const {
 	std::cout << "(" << m_x << ", " << m_y << ")" << "\n";
 }
 
-double Point2d::distance(Point2d& point2) const {
+double Point2d::distanceTo(Point2d& point2) const {
 	double x_diff = m_x - point2.getX();
 	double x_diff_squared = pow(x_diff, 2);
 	
@@ -24,8 +24,12 @@ double Point2d::distance(Point2d& point2) const {
 int main() {
 	Point2d point1;
 	Point2d point2(3.0, 4.0);
-	point1.print();
-	point2.print();
+	
+	std::cout << "point 1: "; point1.print();
+	std::cout << "point 2: "; point2.print();
+	
+	std::cout << "distance from point 1 to point 2: "
+			  << point1.distanceTo(point2) << "\n";
 }
 
 
