596d93acdb9a8e6fd67f9da09352805ae71bf1c1
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-07-26 08:33:13 -0500

    implementing print()

--- Chapter_8/Quizzes/Monster/monster.cpp
+++ Chapter_8/Quizzes/Monster/monster.cpp
@@ -6,6 +6,12 @@
 #include <iostream>
 #include <string>
 #include "monster.h"
+
+Monster::print() const {
+    std::string m_type_string = getTypeString(m_type);
+    std::cout << m_name << "the " << m_type_string << " has "
+              << m_hp << " hitpoints and says " << m_roar << "\n";
+}
  
 int main() {
     Monster skeleton(MonsterType::Skeleton, "Bones", "rattle", 4);
