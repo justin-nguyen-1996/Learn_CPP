10679fad7d58e366414f5afd0414d4a87485e10c
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-07-26 08:27:59 -0500

    added main()

---
+++ Chapter_8/Quizzes/Monster/monster.cpp
@@ -0,0 +1,13 @@
+/*
+ * Author:  Justin Nguyen
+ * Created: 7/26/2016
+ */
+ 
+#include <iostream>
+#include <string>
+#include "monster.h"
+ 
+int main() {
+    Monster skeleton(MonsterType::Skeleton, "Bones", "rattle", 4);
+}
+
