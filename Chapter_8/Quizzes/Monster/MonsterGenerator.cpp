64a8eabab8e4a024ecbb34944a97367ef02eb24e
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-07-27 09:17:29 -0500

    forgot to make it static

--- Chapter_8/Quizzes/Monster/MonsterGenerator.cpp
+++ Chapter_8/Quizzes/Monster/MonsterGenerator.cpp
@@ -3,13 +3,13 @@
  * Created: 7/27/2016
  */
  
-MonsterGenerator::generateMonster() {
+static MonsterGenerator::generateMonster() {
     return Monster(Monster::Skeleton, "Bones", "rattle", 4);
 }
 
 // Generate a random number between min and max (inclusive)
 // Assumes srand() has already been called
-int getRandomNumber(int min, int max) {
+static int MonsterGenerator::getRandomNumber(int min, int max) {
     
     // static used for efficiency, so we only calculate this value once
 	static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
