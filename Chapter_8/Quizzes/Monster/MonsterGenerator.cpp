f5a2c02a10fa123d202bf1fcd4e5f0227943de51
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-07-27 09:30:40 -0500

    making generateMonster random

--- Chapter_8/Quizzes/Monster/MonsterGenerator.cpp
+++ Chapter_8/Quizzes/Monster/MonsterGenerator.cpp
@@ -4,16 +4,16 @@
  */
  
 static MonsterGenerator::generateMonster() {
-    return Monster(Monster::Skeleton, "Bones", "rattle", 4);
+	MonsterType type = MonsterType[getRandomNumber(0, MAX_MONSTER_TYPES - 1)];
+	
+    return Monster(type, "Bones", "rattle", 4);
 }
 
-// Generate a random number between min and max (inclusive)
-// Assumes srand() has already been called
+/* Generate a random number between min and max (inclusive)
+ * Assumes srand() has already been called */
 static int MonsterGenerator::getRandomNumber(int min, int max) {
-    
     // static used for efficiency, so we only calculate this value once
 	static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
-	
 	// evenly distribute the random number across our range
 	return static_cast<int>(rand() * fraction * (max - min + 1) + min);
 }
