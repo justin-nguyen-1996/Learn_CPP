250f9ddf9f340ea8b2b45c4acdcb4a21acdb260b
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-07-27 09:16:57 -0500

    adding function to generate random numbers

--- Chapter_8/Quizzes/Monster/MonsterGenerator.cpp
+++ Chapter_8/Quizzes/Monster/MonsterGenerator.cpp
@@ -7,3 +7,15 @@ MonsterGenerator::generateMonster() {
     return Monster(Monster::Skeleton, "Bones", "rattle", 4);
 }
 
+// Generate a random number between min and max (inclusive)
+// Assumes srand() has already been called
+int getRandomNumber(int min, int max) {
+    
+    // static used for efficiency, so we only calculate this value once
+	static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
+	
+	// evenly distribute the random number across our range
+	return static_cast<int>(rand() * fraction * (max - min + 1) + min);
+}
+	
+	
