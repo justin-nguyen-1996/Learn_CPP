02669bd5ea1cbf18d753f7c296d1fcc7a003091f
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-07-28 09:24:12 -0500

    added enums for card suits and values

--- Chapter_8/Quizzes/Blackjack/Card.h
+++ Chapter_8/Quizzes/Blackjack/Card.h
@@ -7,7 +7,34 @@
 #define CARD_H
 
 class Card {
-
+    private:
+        m_suit;
+        m_value;
+    public:
+        enum CardSuit {
+            CLUBS,
+            DIAMONDS,
+            HEARTS,
+            SPADES,
+            MAX_CARD_SUITS
+        };
+        
+        enum CardValue {
+            TWO,
+            THREE,
+            FOUR,
+            FIVE,
+            SIX,
+            SEVEN,
+            EIGHT,
+            NINE,
+            TEN,
+            JACK,
+            QUEEN,
+            KING,
+            ACE,
+            MAX_CARD_VALUES
+        };
 };
 
 #endif
