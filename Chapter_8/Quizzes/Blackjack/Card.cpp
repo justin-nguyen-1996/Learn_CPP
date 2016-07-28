cfae27891d19bf5203c314e3ec7a1eae6359e1be
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-07-28 09:51:28 -0500

    conversions between enums to strings

--- Chapter_8/Quizzes/Blackjack/Card.cpp
+++ Chapter_8/Quizzes/Blackjack/Card.cpp
@@ -4,9 +4,43 @@
  */
 
 #include <iostream>
+#include <string>
 #include "Card.h"
 
-int main() {
+char Card::getSuitString() {
+    switch (m_suit) {
+        case CLUBS:        return 'C';
+        case DIAMONDS:     return 'D';
+        case HEARTS:       return 'H';
+        case SPADES:       return 'S';
+        case default:      return '?';
+    }
+}
 
+char Card::getValueString() {
+    switch (m_value) {
+        case TWO:          return '2';
+        case THREE:        return '3';
+        case FOUR:         return '4';
+        case FIVE:         return '5';
+        case SIX:          return '6';
+        case SEVEN:        return '7';
+        case EIGHT:        return '8';
+        case NINE:         return '9';
+        case TEN:          return 'T';
+        case JACK:         return 'J';
+        case QUEEN:        return 'Q';
+        case KING:         return 'K';
+        case ACE:          return 'A';
+        case default:      return '?';
+    }
+}
+
+void Card::printCard() {
+    
+}
+
+int main() {
+    
 }
 
