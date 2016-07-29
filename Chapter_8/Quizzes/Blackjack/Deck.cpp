436bf174dfd0affb9bd97332a741424054ae43d5
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-07-29 14:58:55 -0500

    Update Deck.cpp

--- Chapter_8/Quizzes/Blackjack/Deck.cpp
+++ Chapter_8/Quizzes/Blackjack/Deck.cpp
@@ -5,9 +5,9 @@
 
 static int Deck::getRandomNumber(int min, int max) const {
     // static used for efficiency, so we only calculate this value once
-	static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
- 	// evenly distribute the random number across our range
-	return static_cast<int>(rand() * fraction * (max - min + 1) + min);
+    static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
+    // evenly distribute the random number across our range
+    return static_cast<int>(rand() * fraction * (max - min + 1) + min);
 }
 
 static void Deck::swapCard(Card& a, Card& b) {
