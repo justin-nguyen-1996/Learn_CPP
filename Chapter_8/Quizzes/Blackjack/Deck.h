d7058e4ae33cdecc23394485cbdedeb1a5917cc8
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-07-29 14:25:28 -0500

    added deck constructor

--- Chapter_8/Quizzes/Blackjack/Deck.h
+++ Chapter_8/Quizzes/Blackjack/Deck.h
@@ -6,8 +6,24 @@
 #ifndef DECK_H
 #define DECK_H
 
-class Deck {
+#include <array>
 
+class Deck {
+    private:
+        std::array<Card, 52> m_deck;
+    
+    public:
+        Deck() {
+            for (int suit_i = 0; suit_i < CardSuit::MAX_CARD_SUITS; ++suit_i) {
+                for (int rank_i = 0; rank_i < CardValue::MAX_CARD_VALUES; ++rank_i) {
+                    CardSuit suit = CardSuit[suit_i];
+                    CardValue value = CardValue[rank_i];
+                    m_deck[suit_i][rank_i] = Card(suit, rank);
+                }
+            }
+        }
+        
+        
 };
 
 #endif
