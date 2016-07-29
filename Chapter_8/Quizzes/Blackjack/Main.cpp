a6bb1ad610380bc584a7d2f003a1b05f2d52c78a
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-07-29 14:51:34 -0500

    testing shuffle deck and print deck

--- Chapter_8/Quizzes/Blackjack/Main.cpp
+++ Chapter_8/Quizzes/Blackjack/Main.cpp
@@ -8,13 +8,18 @@
 #include "Card.h"
  
 int main() {
+	srand(static_cast<unsigned int>(time(0))); // set initial seed value to system clock
+	
     const Card queen_hearts (CardSuit::HEARTS, CardValue::QUEEN);
     queen_hearts.printCard();
     
     const Card two_spades (CardValue::TWO, CardSuit::SPADES);
     two_spades.printCard();
     
-    srand(static_cast<unsigned int>(time(0))); // set initial seed value to system clock
-    Deck 
+    Deck deck;
+    deck.printDeck();
+    
+    deck.shuffleDeck
+    deck.printDeck();
 }
 
