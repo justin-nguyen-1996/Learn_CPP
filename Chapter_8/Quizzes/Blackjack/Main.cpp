3f8b887e5d7aa1326da3bcc0d096eced24beee90
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-07-29 15:01:04 -0500

    testing dealCard()

--- Chapter_8/Quizzes/Blackjack/Main.cpp
+++ Chapter_8/Quizzes/Blackjack/Main.cpp
@@ -8,18 +8,19 @@
 #include "Card.h"
  
 int main() {
-	srand(static_cast<unsigned int>(time(0))); // set initial seed value to system clock
+    srand(static_cast<unsigned int>(time(0))); // set initial seed value to system clock
 	
     const Card queen_hearts (CardSuit::HEARTS, CardValue::QUEEN);
     queen_hearts.printCard();
-    
     const Card two_spades (CardValue::TWO, CardSuit::SPADES);
     two_spades.printCard();
     
     Deck deck;
     deck.printDeck();
-    
     deck.shuffleDeck
     deck.printDeck();
+    
+    std::cout << "First card is: " << deck.dealCard().printCard() << "\n";
+    std::cout << "Second card is: " << deck.dealCard().printCard() << "\n";
 }
 
