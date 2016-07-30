2dabb2b0dd21537b69f5e2446acfc20b093a4e24
Justin Nguyen <2014justinnguyen@gmail.com>
2016-07-29 21:12:25 -0500

    some enum difficulties

--- Chapter_8/Quizzes/Monster/Monster.h
+++ Chapter_8/Quizzes/Monster/Monster.h
@@ -2,19 +2,32 @@
  * Author:  Justin Nguyen
  * Created: 7/26/2016
  */
- 
+
 #ifndef MONSTER_H
 #define MONSTER_H
 
 #include <string>
 
 class Monster {
+	public:
+        enum MonsterType {
+            Dragon,
+            Goblin,
+            Ogre,
+            Orc,
+            Skeleton,
+            Troll,
+            Vampire,
+            Zombie,
+            NO_TYPE
+        };
+
     private:
         MonsterType m_type = MonsterType::NO_TYPE;
         std::string m_name = "";
         std::string m_roar = "";
         int m_hp = 0;
-        
+
         std::string getTypeString() const {
             switch (m_type) {
                 case Dragon:     return "Dragon";
@@ -26,27 +39,16 @@ class Monster {
                 case Vampire:    return "Vampire";
                 case Zombie:     return "Zombie";
                 case NO_TYPE:    return "NO_TYPE";
-                case default:    return "NO_TYPE";
+                default:    	 return "NO_TYPE";
             }
         }
 
     public:
-        enum class MonsterType {
-            Dragon, 
-            Goblin, 
-            Ogre, 
-            Orc, 
-            Skeleton, 
-            Troll, 
-            Vampire, 
-            Zombie,
-            NO_TYPE
-        }
-        
+
         Monster() {}
         Monster(MonsterType type, std::string name, std::string roar, int hp)
             : m_type(type), m_name(name), m_roar(roar), m_hp(hp) {}
-        
+
         void print() const;
 };
 
