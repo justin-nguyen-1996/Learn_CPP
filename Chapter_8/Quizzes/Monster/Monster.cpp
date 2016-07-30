4fa981f3a993f33487fcce4587c1464e0ab370ad
Justin Nguyen <2014justinnguyen@gmail.com>
2016-07-29 23:10:25 -0500

    seems to be working

--- Chapter_8/Quizzes/Monster/Monster.cpp
+++ Chapter_8/Quizzes/Monster/Monster.cpp
@@ -2,14 +2,15 @@
  * Author:  Justin Nguyen
  * Created: 7/26/2016
  */
- 
+
 #include <iostream>
 #include <string>
 #include "Monster.h"
+#include "MonsterGenerator.h"
 
 void Monster::print() const {
-    std::string m_type_string = getTypeString(m_type);
-    std::cout << m_name << "the " << m_type_string << " has "
+    std::string m_type_string = getTypeString();
+    std::cout << m_name << " the " << m_type_string << " has "
               << m_hp << " hitpoints and says " << m_roar << "\n";
 }
 
@@ -27,11 +28,37 @@ std::string Monster::getTypeString() const {
         default:    	 return "NO_TYPE";
     }
 }
- 
+// int MonsterGenerator::getRandomNumber(int min, int max) {
+    // static used for efficiency, so we only calculate this value once
+// 	static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
+	// evenly distribute the random number across our range
+// 	return static_cast<int>(rand() * fraction * (max - min + 1) + min);
+// }
+// Monster MonsterGenerator::generateMonster() {
+	// only initialize these arrays once
+// 	static std::string s_names[6]
+// 		{ "Blarg", "Moog", "Pksh", "Tyrn", "Mort", "Hans" };
+// 	static std::string s_roars[6]
+// 		{ "*ROAR*", "*peep*", "*squeal*", "*whine*", "*hum*", "*burp*" };
+//
+	// randomize the Monster type, name, roar, and health
+// 	int random_type_num = getRandomNumber(0, Monster::MAX_MONSTER_TYPES - 1);
+// 	Monster::MonsterType type
+// 		= static_cast<Monster::MonsterType>(random_type_num);
+// 	int random_name_num = getRandomNumber(0, 5);
+// 	std::string name = s_names[random_name_num];
+// 	int random_roar_num = getRandomNumber(0, 5);
+// 	std::string roar = s_roars[random_roar_num];
+// 	int hp = getRandomNumber(1, 100);
+//
+//     return Monster(type, name, roar, hp);
+// }
+
 int main() {
     srand(static_cast<unsigned int>(time(0))); // set initial seed value to system clock
-    
-    Monster m = MonsterGenerator();
+
+    Monster m = MonsterGenerator::generateMonster();
     m.print();
+    std::cout << MonsterGenerator::getRandomNumber(0,2) << "\n";
 }
 
--- Chapter_8/Quizzes/Monster/MonsterGenerator.cpp
+++ Chapter_8/Quizzes/Monster/MonsterGenerator.cpp
@@ -7,23 +7,20 @@
 #include "MonsterGenerator.h"
 
 Monster MonsterGenerator::generateMonster() {
-	/* only initialize these arrays once */
+	// only initialize these arrays once
 	static std::string s_names[6]
 		{ "Blarg", "Moog", "Pksh", "Tyrn", "Mort", "Hans" };
 	static std::string s_roars[6]
 		{ "*ROAR*", "*peep*", "*squeal*", "*whine*", "*hum*", "*burp*" };
 
-	/* randomize the Monster type, name, roar, and health */
+	// randomize the Monster type, name, roar, and health
 	int random_type_num = getRandomNumber(0, Monster::MAX_MONSTER_TYPES - 1);
 	Monster::MonsterType type
 		= static_cast<Monster::MonsterType>(random_type_num);
-
 	int random_name_num = getRandomNumber(0, 5);
 	std::string name = s_names[random_name_num];
-
 	int random_roar_num = getRandomNumber(0, 5);
 	std::string roar = s_roars[random_roar_num];
-
 	int hp = getRandomNumber(1, 100);
 
     return Monster(type, name, roar, hp);
@@ -31,7 +28,7 @@ Monster MonsterGenerator::generateMonster() {
 
 /* Generate a random number between min and max (inclusive)
  * Assumes srand() has already been called */
-static int MonsterGenerator::getRandomNumber(int min, int max) {
+int MonsterGenerator::getRandomNumber(int min, int max) {
     // static used for efficiency, so we only calculate this value once
 	static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
 	// evenly distribute the random number across our range
