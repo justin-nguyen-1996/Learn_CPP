c82ddd7ec1555d1553ac93e87d4d7cc534ca9ae8
Justin Nguyen <2014justinnguyen@gmail.com>
2016-07-30 21:21:56 -0500

    minor errors

--- Chapter_8/Quizzes/Blackjack/Card.h
+++ Chapter_8/Quizzes/Blackjack/Card.h
@@ -27,7 +27,7 @@ class Card {
     public:
         Card() {}
         Card(CardSuit suit, CardValue value) : m_suit(suit), m_value(value) {}
-        Card(CardValue value, CardValue suit) { Card(suit, value); }
+        Card(CardValue value, CardSuit suit) { Card(suit, value); }
 
         CardSuit getSuit() const { return m_suit; }
         CardValue getValue() const { return m_value; }
--- Chapter_8/Quizzes/Blackjack/Main.cpp
+++ Chapter_8/Quizzes/Blackjack/Main.cpp
@@ -10,17 +10,24 @@
 int main() {
     srand(static_cast<unsigned int>(time(0))); // set initial seed value to system clock
 	
-    const Card queen_hearts (CardSuit::HEARTS, CardValue::QUEEN);
+    const Card queen_hearts 
+        (Card::CardSuit::HEARTS, Card::CardValue::QUEEN);
     queen_hearts.printCard();
-    const Card two_spades (CardValue::TWO, CardSuit::SPADES);
+    const Card two_spades
+        (Card::CardValue::TWO, Card::CardSuit::SPADES);
     two_spades.printCard();
     
     Deck deck;
     deck.printDeck();
-    deck.shuffleDeck
+    deck.shuffleDeck();
     deck.printDeck();
     
-    std::cout << "First card is: " << deck.dealCard().printCard() << "\n";
-    std::cout << "Second card is: " << deck.dealCard().printCard() << "\n";
+    std::cout << "First card is: ";
+    deck.dealCard().printCard();
+    std::cout << "\n";
+    
+    std::cout << "Second card is: ";
+    deck.dealCard().printCard();
+    std::cout << "\n";
 }
 
