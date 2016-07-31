c4d8c06e664d8aac52780f90d03b850a0ba7fa05
Justin Nguyen <2014justinnguyen@gmail.com>
2016-07-30 22:56:20 -0500

    testing Deck() constructor

--- Chapter_8/Quizzes/Blackjack/Main.cpp
+++ Chapter_8/Quizzes/Blackjack/Main.cpp
@@ -10,21 +10,21 @@
 int main() {
     srand(static_cast<unsigned int>(time(0))); // set initial seed value to system clock
 	
-    const Card queen_hearts 
-        (Card::CardSuit::HEARTS, Card::CardValue::QUEEN);
-    queen_hearts.printCard();
-    const Card two_spades 
-        (Card::CardSuit::SPADES, Card::CardValue::TWO);
-    two_spades.printCard();
-    const Card two_spades2
-        (Card::CardValue::TWO, Card::CardSuit::SPADES);
-    two_spades2.printCard();
+//     const Card queen_hearts 
+//         (Card::CardSuit::HEARTS, Card::CardValue::QUEEN);
+//     queen_hearts.printCard();
+//     const Card two_spades 
+//         (Card::CardSuit::SPADES, Card::CardValue::TWO);
+//     two_spades.printCard();
+//     const Card two_spades2
+//         (Card::CardValue::TWO, Card::CardSuit::SPADES);
+//     two_spades2.printCard();
     
-//     Deck deck;
-//     deck.printDeck();
+    Deck deck;
+    deck.printDeck();
 //     deck.shuffleDeck();
 //     deck.printDeck();
-//     
+    
 //     std::cout << "First card is: ";
 //     deck.dealCard().printCard();
 //     std::cout << "\n";
