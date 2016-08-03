a5777e564104648d83003357594d74adfacba434
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-08-03 14:39:02 -0500

    Update Main.cpp

--- Chapter_9/Lectures/Quizzes/GradeMap/Main.cpp
+++ Chapter_9/Lectures/Quizzes/GradeMap/Main.cpp
@@ -8,6 +8,11 @@
 #include "GradeMap.h"
 
 int main() {
+    GradeMap grades;
+    grades["Joe"] = 'A';
+    grades["Jon"] = 'F';
     
+    std::cout << "Joe has a grade of " << grades["Joe"] << "\n";
+    std::cout << "Jon has a grade of " << grades["Jon"] << "\n";
 }
 
