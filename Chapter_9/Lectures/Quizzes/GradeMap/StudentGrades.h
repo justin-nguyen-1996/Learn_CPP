8f222961171154ded4dda7efe31d23d612a1268f
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-08-03 13:41:52 -0500

    Create StudentGrades.h

---
+++ Chapter_9/Lectures/Quizzes/GradeMap/StudentGrades.h
@@ -0,0 +1,16 @@
+/*
+ * Author:  Justin Nguyen
+ * Created: 8/3/2016
+ */
+ 
+#ifndef STUDENT_GRADES_H
+#define STUDENT_GRADES_H
+
+#include <string>
+
+class StudentGrades {
+    std::string name;
+    char grade;
+};
+
+#endif
