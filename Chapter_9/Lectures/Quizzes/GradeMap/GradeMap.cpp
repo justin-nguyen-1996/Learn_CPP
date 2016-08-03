e79cce9402266d82147f7017c3033959595427a3
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-08-03 14:22:04 -0500

    check if student is already on the list

--- Chapter_9/Lectures/Quizzes/GradeMap/GradeMap.cpp
+++ Chapter_9/Lectures/Quizzes/GradeMap/GradeMap.cpp
@@ -7,6 +7,15 @@
 #include "GradeMap.h"
 
 char& GradeMap::operator[] (const std::string& name) {
+    // check to see if the student is already on the list
+    bool found_student = false;
+    for (const auto& student : m_map) {
+        if (student.name == name) {
+            found_student = true;
+            break;
+        }
+    }
+    
     
 }
 
