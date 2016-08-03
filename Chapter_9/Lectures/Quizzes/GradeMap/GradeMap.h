108a7ba1d28f9f8ff36c03aff53378658eef7503
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-08-03 13:44:46 -0500

    Create GradeMap.h

---
+++ Chapter_9/Lectures/Quizzes/GradeMap/GradeMap.h
@@ -0,0 +1,18 @@
+/*
+ * Author:  Justin Nguyen
+ * Created: 8/3/2016
+ */
+ 
+#ifndef GRADE_MAP_H
+#define GRADE_MAP_H
+
+#include <vector>
+
+class GradeMap {
+    private:
+        std::vector<StudentGrades> m_map;
+    public:
+        GradeMap() {}
+};
+
+#endif
