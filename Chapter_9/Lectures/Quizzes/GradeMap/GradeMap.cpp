050c0f45d82d1f8d28c1e58c2feb468ea6c43026
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-08-03 14:35:49 -0500

    add new student

--- Chapter_9/Lectures/Quizzes/GradeMap/GradeMap.cpp
+++ Chapter_9/Lectures/Quizzes/GradeMap/GradeMap.cpp
@@ -10,12 +10,14 @@ char& GradeMap::operator[] (const std::string& name) {
     // check to see if the student is already on the list
     bool found_student = false;
     for (const auto& student : m_map) {
-        if (student.name == name) {
-            found_student = true;
-            break;
+        if (student.name == name) { // found the student
+            return student.grade;
         }
     }
     
-    
+    // did not find the student
+    StudentGrades new_student(name, '');
+    m_map.push_back(new_student);
+    return m_map.back();
 }
 
