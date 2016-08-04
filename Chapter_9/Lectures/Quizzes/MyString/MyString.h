c6d20267062711e4ae9d09d975dc7d1a770637c1
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-08-04 11:22:40 -0500

    Create MyString.h

---
+++ Chapter_9/Lectures/Quizzes/MyString/MyString.h
@@ -0,0 +1,16 @@
+/*
+ * Author:  Justin Nguyen
+ * Created: 8/04/16
+ */
+ 
+#ifndef MY_STRING_H
+#define MY_STRING_H
+
+#include <string>
+
+class MyString {
+    private:
+        std::string m_string;
+};
+
+#endif
