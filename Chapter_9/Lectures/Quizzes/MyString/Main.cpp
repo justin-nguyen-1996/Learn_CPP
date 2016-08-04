881e0848cc7614e3bef11605a8a4bea167550293
justin-nguyen-1996 <2014justinnguyen@gmail.com>
2016-08-04 11:31:58 -0500

    Create Main.cpp

---
+++ Chapter_9/Lectures/Quizzes/MyString/Main.cpp
@@ -0,0 +1,14 @@
+/*
+ * Author:  Justin Nguyen
+ * Created: 8/04/16
+ */
+ 
+#include <iostream>
+#include <string>
+#include "MyString.h"
+
+int main() {
+    MyString my_string("Hello World!");
+    std::cout << my_string(6, 5) << "\n";
+}
+
